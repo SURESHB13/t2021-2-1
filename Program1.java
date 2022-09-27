da082c6f121e85186593b15c709a5e194a977faa
Suresh Biradar <sureshbiradar55@gmail.com>
2022-09-27 09:38:42 +0530

    Program1 added

--- Program1.java
+++ Program1.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
 
 public class Program1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter two numbers");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		scanner.nextLine();
+		System.out.println("enter the type of operation");
+		String operarationString = scanner.nextLine().trim().toLowerCase();
+		try {
+			if (operarationString.equalsIgnoreCase("addition")) {
+				Double c = a + b;
+				System.out.println("addition of two number is: " + c);
+
+			} else if (operarationString.equalsIgnoreCase("substraction")) {
+				Double c = a - b;
+				System.out.println("substraction of two number is: " + c);
+
+			} else if (operarationString.equalsIgnoreCase("multiplication")) {
+				Double c = a * b;
+				System.out.println("multiplication of two number is: " + c);
+
+			} else if (operarationString.equalsIgnoreCase("division")) {
+				Double c = a / b;
+				System.out.println("division of two number is: " + c);
+
+			} else {
+				System.out.println(
+						"enter the type of operation correctly ex addition,substraction,multiplication and division");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
